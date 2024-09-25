d083eda3607d6cbfe705fa9ed3e2862834e5910d
Marcie. M <163384465+mars-hm@users.noreply.github.com>
2024-09-26 01:15:06 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,33 @@
+import streamlit as st
+import tensorflow as tf
+from PIL import Image
+import numpy as np
+
+model = tf.keras.models.load_model('cnn_tumor.h5')
+
+def preprocess_image(image):
+    image = image.resize((128, 128))
+    test = np.array(image) / 255.0  # Normalize to [0, 1]
+    test = np.expand_dims(test, axis=0)
+    return test
+
+def predict_tumor(test):
+    prediction = model.predict(test)
+    if prediction > 0.5:
+        return "Tumor Detected"
+    else: 
+        return "No Tumor Detected"
+
+# Streamlit app
+st.title("Brain Tumor Detection using CNN")
+
+uploaded_file = st.file_uploader("Upload an Image:", type="jpg")
+
+if uploaded_file:
+    image = Image.open(uploaded_file).convert('RGB')
+    img_array = preprocess_image(image)
+    result = predict_tumor(img_array)
+    
+    # Display the uploaded image and prediction result
+    st.image(image, caption='Uploaded Image', use_column_width=True)
+    st.info(f"Prediction: {result}")
